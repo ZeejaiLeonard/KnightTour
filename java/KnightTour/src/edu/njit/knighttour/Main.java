d136085128be3df88354bd077106e6ded36e38d8
Zeejai Leonard <zeejai.leonard@gmail.com>
2020-06-12 15:00:02 -0700

    increased java readibility

--- java/KnightTour/src/edu/njit/knighttour/Main.java
+++ java/KnightTour/src/edu/njit/knighttour/Main.java
@@ -17,51 +17,51 @@ public class Main {
 
 class Chessboard {
 
-    KnightSquare[][] board = new KnightSquare[8][8];
-    public int START = -1;
-    public int BEFORE = 0;
-    public int TEMP = 99;
-    public int AFTER = 100;
+    KnightSquare[][] board = new KnightSquare[8][8]; //create a chessboard
+    public int START = -1; //starting status
+    public int BEFORE = 0; //hasn't been tried
+    public int TEMP = 99; //currently trying
+    public int AFTER = 100; //works
 
     public Chessboard(){
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
-                board[i][j] = new KnightSquare(i, j);
+                board[i][j] = new KnightSquare(i, j); //populate chessboard with squares
             }
         }
-        board[0][0].status = START;
+        board[0][0].status = START; //set the top left as the starting square
     }
 
     public boolean solve(){
         return solve(0, 0, 0);
-    }
+    } //initial call
 
-    public boolean solve(int x, int y, int n){
+    public boolean solve(int x, int y, int n){ //recursive solution
         boolean found = false;
         int i;
         int j;
-        if(board[x][y].status == TEMP || board[x][y].status == AFTER){
+        if(board[x][y].status == TEMP || board[x][y].status == AFTER){ //if this square has already been tried or assigned
             board[x][y].visit = n;
-            return found;
+            return found; //then this obviously isn't the way to go
         }
-        n++;
-        board[x][y].status = TEMP;
-        board[x][y].visit = n;
-        if(board[x][y].status == START || n == 63){
-            found = true;
-            board[x][y].visit = n;
-            board[x][y].status = AFTER;
+        n++; //increment path number
+        board[x][y].status = TEMP; //set square status to trying
+        board[x][y].visit = n; //give this board a number
+        if(board[x][y].status == START || n == 63){ //if it's back to the starting square or it reached the end of the path
+            found = true; //we've found a solution
+            board[x][y].visit = n; //give this square a number on the path
+            board[x][y].status = AFTER; //set square status to done
             return found;
-        }else{
-            for(int q = 0; q < 8; q++){
-                if(board[x][y].possibilities[q] != null){
+        }else{ //otherwise if we aren't at the end
+            for(int q = 0; q < 8; q++){ //try the max 8 possibilities from this square
+                if(board[x][y].possibilities[q] != null){ //if there is a possibility
                     i = board[x][y].possibilities[q].x;
                     j = board[x][y].possibilities[q].y;
-                    if(board[i][j].status == BEFORE){
-                        found = solve(i, j, n);
-                        if(found){
-                            board[x][y].visit = n;
-                            board[x][y].status = AFTER;
+                    if(board[i][j].status == BEFORE){ //if this possibility hasn't been tried before
+                        found = solve(i, j, n); //recursive call with this possibility
+                        if(found){ //if this possibility works
+                            board[x][y].visit = n; //give this square a number on the path
+                            board[x][y].status = AFTER; //set square status to done
                             return found;
                         }
                     }
@@ -69,10 +69,10 @@ class Chessboard {
 
             }
         }
-        return found;
+        return found; //all the possibilities didn't work, backtrack
     }
 
-    public void printBoard(){
+    public void printBoard(){ //print the chessboard with path
         for(int i = 0; i < 8; i++){
             for(int j = 0; j < 8; j++){
                 if(this.board[i][j].visit < 10){
@@ -88,57 +88,59 @@ class Chessboard {
 
 class KnightSquare {
 
-    Position p;
-    int visit;
-    int status;
-    Position[] possibilities;
+    Position p; //position of this square on the board
+    int visit; //record number of this square on the path
+    int status; //the status of this square
+    Position[] possibilities; //the possible knight moves from this square
 
     public KnightSquare(int x, int y){
-        p = new Position(x, y);
-        visit = 0;
-        status = 0;
-        possibilities = findPossibilities();
+        p = new Position(x, y); //give this square it's position
+        visit = 0; //it hasn't been visited
+        status = 0; //it hasn't been tried
+        possibilities = findPossibilities(); //give the list of possible knight moves from this square
     }
 
     public Position[] findPossibilities(){
-        Position[] poss = new Position[8];
-        int n = 0;
-        if(this.p.x + 1 <= 7){
-            if(this.p.y + 2 <= 7){
+        Position[] poss = new Position[8]; //maximum of 8 possible moves
+        int n = 0; //start index at 0
+        //if valid, add this position to the list of possibilities
+        //increment index
+        if(this.p.x + 1 <= 7){ //right 1
+            if(this.p.y + 2 <= 7){ //down 2
                 poss[n] = (new Position(this.p.x + 1, this.p.y + 2));
                 n++;
             }
-            if(this.p.y - 2 >= 0){
+            if(this.p.y - 2 >= 0){ //up 2
                 poss[n] = (new Position(this.p.x + 1, this.p.y - 2));
                 n++;
             }
         }
-        if(this.p.x + 2 <= 7){
-            if(this.p.y + 1 <= 7){
+        if(this.p.x + 2 <= 7){ //right 2
+            if(this.p.y + 1 <= 7){ //down 1
                 poss[n] = (new Position(this.p.x + 2, this.p.y + 1));
                 n++;
             }
-            if(this.p.y - 1 >= 0){
+            if(this.p.y - 1 >= 0){ //up 1
                 poss[n] = (new Position(this.p.x + 2, this.p.y - 1));
                 n++;
             }
         }
-        if(this.p.x - 1 >= 0){
-            if(this.p.y + 2 <= 7){
+        if(this.p.x - 1 >= 0){ //left 1
+            if(this.p.y + 2 <= 7){ //down 2
                 poss[n] = (new Position(this.p.x - 1, this.p.y + 2));
                 n++;
             }
-            if(this.p.y - 2 >= 0){
+            if(this.p.y - 2 >= 0){ //up 2
                 poss[n] = (new Position(this.p.x - 1, this.p.y - 2));
                 n++;
             }
         }
-        if(this.p.x - 2 >= 0){
-            if(this.p.y + 1 <= 7){
+        if(this.p.x - 2 >= 0){ //left 2
+            if(this.p.y + 1 <= 7){ //down 1
                 poss[n] = (new Position(this.p.x - 2, this.p.y + 1));
                 n++;
             }
-            if(this.p.y - 1 >= 0){
+            if(this.p.y - 1 >= 0){ //up 1
                 poss[n] = (new Position(this.p.x - 2, this.p.y - 1));
                 n++;
             }
